8facab73ca5ba581abce91cbaf7a5435b7327bac
Lương Quốc Thái <luongquocthai.thaigo.2003@gmail.com>
2025-01-18 15:59:32 +0700

    get_by_id function done

--- MenuAndStatisticsManagementService_Django/menu_management/urls.py
+++ MenuAndStatisticsManagementService_Django/menu_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MenuViewSet
 
 urlpatterns = [
-    path('menus', MenuViewSet.as_view({'get': 'list','post':'create' })),
-    #path('tables/<int:pk>', TableViewSet.as_view({'get': 'get_by_id', 'put': 'update_by_id', 'delete': 'delete'})),
-    #path('tables/search/', TableViewSet.as_view({'get': 'search'})),  # Đảm bảo rằng endpoint này có mặt
+    path('menus', MenuViewSet.as_view({'get': 'list','post':'create','delete':'delete_many' })),
+    path('menus/<str:pk>', MenuViewSet.as_view({'get': 'get_by_id', 'put': 'update_by_id', 'delete': 'delete'})),
+    path('menus/search/', MenuViewSet.as_view({'get': 'search'})),  # Đảm bảo rằng endpoint này có mặt
 ]
